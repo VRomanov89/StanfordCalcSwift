268940283760e946e22e42bb001939dc29085e06
Vladimir Romanov <vladromweb@gmail.com>
2016-03-16 21:38:36 -0400

    Added operand evaluation function into the Model
    Added operand evaluation function into the Model

--- StanfordCalcSwift/StanfordCalcSwift/CalculatorBrain.swift
+++ StanfordCalcSwift/StanfordCalcSwift/CalculatorBrain.swift
@@ -24,7 +24,32 @@ class CalculatorBrain {
         knownOps["-"] = Op.BinaryOperation("-", {$1  - $0})
         knownOps["+"] = Op.BinaryOperation("+", {$1  + $1})
         knownOps["sqrt"] = Op.UnaryOperation("sqrt", {sqrt($0)})
-        
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if(!ops.isEmpty) {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+            
+        }
+        return (nil, ops)
     }
     
     func pushOperand(operand: Double) {
